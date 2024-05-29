171aaf27486d40c2fe59db7b6ada6a853ce274fa
AJMAL M T <ajmalmt67@gmail.com>
2024-05-29 12:00:34 +0530

    add loading using next js

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,8 @@ const Home = async ({ searchParams }) => {
             genre === "fetchTopRated"
                 ? "/movie/top_rated"
                 : "/trending/all/week"
-        }?api_key=${API_KEY}&language=en-US&page=1`
+        }?api_key=${API_KEY}&language=en-US&page=1`,
+        { next: { revalidate: 10000 } }
     );
     const data = await res.json();
     if (!res.ok) {
