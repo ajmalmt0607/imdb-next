5232f810e437a732295729736d7e20e9958c2a07
AJMAL M T <ajmalmt67@gmail.com>
2024-05-30 10:46:58 +0530

    add movie page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ import { IoMdThumbsUp } from "react-icons/io";
 
 function Card({ result }) {
     return (
-        <div className="group cursor-pointer sm:hover:shadow-slate-400 sm:m-2 transition-shadow duration-200">
+        <div className="group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border-slate-400 sm:border sm:m-2 transition-shadow duration-200">
             <Link href={`/movie/${result.id}`}>
                 <Image
                     src={`https://image.tmdb.org/t/p/original/${
